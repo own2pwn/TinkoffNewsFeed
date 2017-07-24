049d3532877d2be2a48cbfcfee479fef51c19663
Unknown <mforall29@gmail.com>
2017-07-24 21:46:06 +0300

    json mapper

--- TinkoffNewsFeed/TinkoffNewsFeed/Core/Network/Parser/News/NewsListParser.swift
+++ TinkoffNewsFeed/TinkoffNewsFeed/Core/Network/Parser/News/NewsListParser.swift
@@ -9,6 +9,33 @@ import SwiftyJSON
 final class NewsListParser: IParser<[NewsEntityModel], JSON> {
     override func parse(_ response: JSON) -> [NewsEntityModel]? {
 
+        let resultCode = response[NEWS_LIST_STATUS_CODE_KEY].string ?? "nil"
+        if resultCode != EXPECTED_STATUS_CODE {
+            log.warning("API response status code: \(resultCode) | Received data may be wrong!")
+        }
+
+        var newsModel = [NewsEntityModel]()
+        let payload = response[NEWS_LIST_PAYLOAD_KEY].arrayValue
+
+        for news in payload {
+            let info = news.dictionary
+            
+        }
+
         return nil
     }
+
+    // MARK: - Private
+    
+    private func mapJSON(_ json: JSON) -> NewsEntityModel? {
+    }
+
+    private typealias JSONType = [String: Any]
+
+    // MARK: - Constants
+
+    private let EXPECTED_STATUS_CODE = "OK"
+
+    private let NEWS_LIST_PAYLOAD_KEY = "resultCode"
+    private let NEWS_LIST_STATUS_CODE_KEY = "resultCode"
 }
