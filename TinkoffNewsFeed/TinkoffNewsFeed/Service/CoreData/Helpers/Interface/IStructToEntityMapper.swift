bcf42a41eac1adb2c9bdddce1415b577058b2c3e
Unknown <mforall29@gmail.com>
2017-07-25 09:08:17 +0300

    documentation

--- TinkoffNewsFeed/TinkoffNewsFeed/Service/CoreData/Helpers/Interface/IStructToEntityMapper.swift
+++ TinkoffNewsFeed/TinkoffNewsFeed/Service/CoreData/Helpers/Interface/IStructToEntityMapper.swift
@@ -6,5 +6,21 @@
 import Foundation
 
 protocol IStructToEntityMapper {
+
+    /**
+     
+     Maps structure values to `object` ones.
+     
+     It dead simple iterates through passed object attributes
+     and uses `setValue` where
+     
+     - `keyPath` is *attribute name*.
+     - `value`   is structure member with *the same* name value or nil if there is no such member.
+     
+     - Parameter model: Struct that conforms to KVC
+     
+     - Parameter object: Generic `NSManagedObject`
+     
+    */
     static func map<T>(_ model: IEntityMappable, _ object: inout T)
 }
