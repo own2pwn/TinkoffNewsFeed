b2507571a45e32f8ddb6d8f77562f17f3f48f205
Unknown <mforall29@gmail.com>
2017-07-27 14:01:04 +0300

    updating existing

--- TinkoffNewsFeed/TinkoffNewsFeed/Core/CoreData/Cache/NewsListCacheManager.swift
+++ TinkoffNewsFeed/TinkoffNewsFeed/Core/CoreData/Cache/NewsListCacheManager.swift
@@ -7,37 +7,17 @@ import Foundation
 import CoreData
 
 final class NewsListCacheManager: INewsListCacheManager {
-    //    func updateCache(for news: [NewsEntityModel]) {
-    //        var newsIds = [String]()
-    //        var newsHashes = [String]()
-    //
-    //        for single in news {
-    //            let id = single.id
-    //            let hash = single.titleHash
-    //
-    //            newsIds.append(id)
-    //            newsHashes.append(hash)
-    //        }
-    //
-    //        let fmt = "id IN %@ AND (NOT (titleHash in %@))"
-    //        let predicate = NSPredicate(format: fmt, newsIds, newsHashes)
-    //
-    //        if let toUpdate = coreDataWorker.get(type: News.self, predicate: predicate, sortDescriptors: nil, fetchLimit: nil) {
-    //            for oldNews in toUpdate {
-    //                var updatedNews = oldNews
-    //                let newNews = news.first(where: { $0.id == oldNews.id! })
-    //                objectMapper.map(newNews!, &updatedNews)
-    //            }
-    //            contextManager.performSave(context: saveContext) { error in
-    //                if let e = error {
-    //                    log.error(e)
-    //                }
-    //            }
-    //        }
-    //    }
-
+    
     func cache(_ payload: [NewsListPayload]) {
         queue.sync { [weak self] in
+            
+            // get saved news where hash is different and update them
+            
+            self?.updateCache(for: payload)
+            
+            // delete already existing news from payload
+            // store new news
+            
             for news in payload {
                 if let strongSelf = self {
                     var mObject = News(context: strongSelf.saveContext)
@@ -55,30 +35,35 @@ final class NewsListCacheManager: INewsListCacheManager {
             }
         }
     }
-
-    func cache(_ data: NewsListAPIModel) {
-        queue.sync { [weak self] in
-            let news = data.payload!
-            for single in news {
-                if let strongSelf = self {
-                    var mObject = News(context: strongSelf.saveContext)
-                    strongSelf.objectMapper.map(single, &mObject)
-                    mObject.titleHash = single.title.sha1()
-                    mObject.viewsCount = 0
-                }
-            }
-            if let strongSelf = self {
-                strongSelf.contextManager.performSave(context: strongSelf.saveContext) { error in
-                    if let e = error {
-                        log.error(e)
-                    }
-                }
+    
+    // MARK: - Private
+    
+    func updateCache(for news: [NewsListPayload]) {
+        var newsIds = Set<String>()
+        var newsHashes = Set<String>()
+        
+        for single in news {
+            let id = single.id
+            let hash = single.title.sha1()
+            
+            newsIds.insert(id)
+            newsHashes.insert(hash)
+        }
+        
+        let fmt = "id IN %@ AND (NOT (titleHash in %@))"
+        let predicate = NSPredicate(format: fmt, newsIds, newsHashes)
+        
+        if let toUpdate = coreDataWorker.get(type: News.self, predicate: predicate) {
+            for oldNews in toUpdate {
+                var updatedNews = oldNews
+                let newNews = news.first(where: { $0.id == oldNews.id! })!
+                objectMapper.map(newNews, &updatedNews)
             }
         }
     }
-
+    
     // MARK: - DI
-
+    
     init(contextManager: ICDContextManager, saveContext: NSManagedObjectContext,
          objectMapper: IStructToEntityMapper.Type, coreDataWorker: ICoreDataWorker) {
         self.contextManager = contextManager
@@ -86,7 +71,7 @@ final class NewsListCacheManager: INewsListCacheManager {
         self.objectMapper = objectMapper
         self.coreDataWorker = coreDataWorker
     }
-
+    
     private let contextManager: ICDContextManager
     private let saveContext: NSManagedObjectContext
     private let objectMapper: IStructToEntityMapper.Type
--- TinkoffNewsFeed/TinkoffNewsFeed/Core/CoreData/Worker/CoreDataWorker.swift
+++ TinkoffNewsFeed/TinkoffNewsFeed/Core/CoreData/Worker/CoreDataWorker.swift
@@ -27,7 +27,27 @@ final class CoreDataWorker: ICoreDataWorker {
         return result?.first
     }
 
-    // TODO: use threads
+    func getDict<T: NSManagedObject>(type: T.Type,
+                                     predicate: NSPredicate? = nil,
+                                     propertiesToFetch: [String]? = nil,
+                                     sortDescriptors: [NSSortDescriptor]? = nil,
+                                     fetchLimit: Int? = nil) -> [Any]? {
+        let name = T.entityName
+        let fr = NSFetchRequest<NSFetchRequestResult>(entityName: name)
+        fr.predicate = predicate
+        fr.sortDescriptors = sortDescriptors
+        fr.propertiesToFetch = propertiesToFetch
+        fr.resultType = .dictionaryResultType
+        fr.returnsDistinctResults = true
+
+        if let limit = fetchLimit {
+            fr.fetchLimit = limit
+        }
+
+        let result = try? context.fetch(fr)
+
+        return result
+    }
 
     func get<T: NSManagedObject>(type: T.Type,
                                  predicate: NSPredicate? = nil,
@@ -37,9 +57,11 @@ final class CoreDataWorker: ICoreDataWorker {
         let fr = NSFetchRequest<T>(entityName: name)
         fr.predicate = predicate
         fr.sortDescriptors = sortDescriptors
+
         if let limit = fetchLimit {
             fr.fetchLimit = limit
         }
+
         let result = try? context.fetch(fr)
 
         return result
@@ -49,6 +71,7 @@ final class CoreDataWorker: ICoreDataWorker {
                                       predicate: NSPredicate? = nil,
                                       sortDescriptors: [NSSortDescriptor]? = nil,
                                       fetchLimit: Int? = nil) -> T? {
+
         return get(type: type, predicate: predicate, sortDescriptors: sortDescriptors, fetchLimit: fetchLimit)?.first
     }
 
