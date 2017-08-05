2f33f943033e964f19a6bb70683841eb2e53a117
Unknown <mforall29@gmail.com>
2017-08-05 09:47:55 +0300

    ext cleanup

--- TinkoffNewsFeed/TinkoffNewsFeed/Core/CoreData/Interface/ICDContextProvider.swift
+++ TinkoffNewsFeed/TinkoffNewsFeed/Core/CoreData/Interface/ICDContextProvider.swift
@@ -11,7 +11,6 @@ protocol ICDContextManager {
     func performSave(context: NSManagedObjectContext,
                      completion: ((Error?) -> Void)?)
 
-    // var masterContext: NSManagedObjectContext { get }
     var mainContext: NSManagedObjectContext { get }
     var saveContext: NSManagedObjectContext { get }
 }
